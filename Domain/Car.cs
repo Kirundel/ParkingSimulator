dbf53e3ec6c9e21c176112ff5c839bfcea93811e
Kirundel <kirill.kaymakov@mail.ru>
2018-09-25 23:50:50 +0400

    Domain project was added.

--- Domain/Car.cs
+++ Domain/Car.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Infrastructure;
 
-namespace ViewModels
+namespace Domain
 {
-    public class ParkingSimulationViewModel : ViewModelBase
+    public class Car
     {
     }
 }
